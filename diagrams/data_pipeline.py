44a0f0b9a99be77fa5889a546f8291e6c5d5dcf7
eli.sutton <elisutton0721@gmail.com>
2022-10-13 12:44:53 -0400

    updated data pipeline diagram to align with new GPX to JSON script

--- diagrams/data_pipeline.py
+++ diagrams/data_pipeline.py
@@ -1,4 +1,4 @@
-from diagrams import Diagram, Cluster, Edge
+from diagrams import Diagram, Cluster
 from diagrams.generic.device import Mobile
 from diagrams.custom import Custom
 
@@ -15,9 +15,9 @@ with Diagram('The Adventures of Eli - Data Pipeline', filename='data_pipeline', 
         iCloud = Custom('iCloud', './my_resources/iCloud-icon.jpeg')
     
     with Cluster('Transformation'):
-        unprocessed_gpx_folder = Custom('Unprocessed GPX', './my_resources/folder-icon.png') 
-        gpx_to_json_scr = Custom('GPX to JSON', './my_resources/shell-script-icon.jpeg')
-        processed_gpx_folder = Custom('Processed GPX', './my_resources/folder-icon.png') 
+        unconverted_gpx_folder = Custom('Unconverted GPX', './my_resources/folder-icon.png') 
+        gpx_to_json_scr = Custom('Batch GPX to JSON', './my_resources/python-script-icon.png')
+        converted_gpx_folder = Custom('Converted GPX', './my_resources/folder-icon.png') 
         unenhanced_json_folder = Custom('Unenhanced JSON', './my_resources/folder-icon.png')
         enhance_json_scr = Custom('Enhance JSON', './my_resources/python-script-icon.png')
         enhanced_json_folder = Custom('Enhanced JSON', './my_resources/folder-icon.png')
@@ -27,10 +27,10 @@ with Diagram('The Adventures of Eli - Data Pipeline', filename='data_pipeline', 
 
     wsp >> iCloud 
     gaia >> iCloud 
-    iCloud >> unprocessed_gpx_folder 
-    unprocessed_gpx_folder >> gpx_to_json_scr 
+    iCloud >> unconverted_gpx_folder 
+    unconverted_gpx_folder >> gpx_to_json_scr 
     gpx_to_json_scr >> [
-        processed_gpx_folder,
+        converted_gpx_folder,
         unenhanced_json_folder
     ] 
     unenhanced_json_folder >> enhance_json_scr >> enhanced_json_folder
